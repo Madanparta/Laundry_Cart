5b4accb5fd2caa43e8d17ca22ebace374bc6c691
Madanparta <partagowda15@gmail.com>
2023-10-01 23:10:15 +0530

    creating order part in serverside with autho

--- backend/Models/order.js
+++ backend/Models/order.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const orderSchema = mongoose.Schema({
+const orderSchema =new mongoose.Schema({
     product_type:{type:String,required:true},
     wash_type:{type:String,required:true},
     price:{type:Number,required:true},
     quantity:{type:Number,required:true},
 })
 
-const productOrderSchema = mongoose.Schema({
-    user:{type:mongoose.type.ObjectId,ref:"users"},
+const productOrderSchema =new mongoose.Schema({
+    user:{type:mongoose.Types.ObjectId,ref:"users"},
     orders:[orderSchema],
     total_price : {type:Number,required:true},
     status:{type:String,required:true}
--- backend/Router/contactsRouter.js
+++ backend/Router/contactsRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const jwt = require("jsonwebtoken");
 const contactRouter = express.Router()
+const Orders = require("../Models/order");
+const Product = require("../Models/product");
 
 
 const verfiyFwt = (req,res,next)=>{
@@ -14,9 +16,55 @@ const verfiyFwt = (req,res,next)=>{
 }
 contactRouter.use(verfiyFwt);
 
-contactRouter.get("/contacts_details",(req,res)=>{
-    console.log(req.user)
-    res.send("contact page..")
+contactRouter.post("/createorder",async (req,res)=>{
+    // console.log(req.user)
+    try{
+        await Orders.create({user:req.user,...req.body});
+        res.status(201).json({
+            status:"success",
+            message:"Data added"
+        })
+
+    }catch(err){
+        res.status(406).json({
+            status:"Failed",
+            message:err.message
+        })
+    }
+})
+
+contactRouter.get("/product",async(req,res)=>{
+    try{
+        const data = await Product.find().sort({_id:-1});
+        res.json(data)
+
+    }catch(err){
+        res.status(406).json({
+            status:"Failed",
+            message:err.message
+        })
+    }
+})
+
+contactRouter.get('/prevorder',async (req,res)=>{
+    try{
+        const order = await Orders.find({user:req.user});
+        res.status(200).json({
+            status:"success",
+            Orders:order
+        })
+    }catch(err){
+        res.status(400).send("Failed");
+    }
+})
+
+contactRouter.put("/updateorder/:id",async (req,res)=>{
+    try{
+        await Orders.findByIdAndUpdate({_id : req.params.id}, { $set:{'status': "Cancelled"}})
+        res.status(200).send("Updated")
+    }catch(err){
+        res.status(401).send(err.message)
+    }
 })
 
 module.exports = contactRouter;
